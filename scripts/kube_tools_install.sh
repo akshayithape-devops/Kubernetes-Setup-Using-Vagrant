bc865a3af875f591c4b41c324bdea466fe01a9dd
Akshay Ithape <82793140+akshayithape-devops@users.noreply.github.com>
2024-03-30 10:00:12 +0530

    Update kube_tools_install.sh

--- scripts/kube_tools_install.sh
+++ scripts/kube_tools_install.sh
@@ -6,8 +6,8 @@ apt-get install -y apt-transport-https ca-certificates curl
 
 # Setup the Repository 
 mkdir -p /etc/apt/keyrings/
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.25/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.25/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Installation of kube tools - kubeadm, kubelet and kubectl
 apt-get update -y
